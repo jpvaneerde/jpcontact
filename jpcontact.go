4f2b7ae47cb1bd573c1fa0a4951f327e2b89dd58
jpvaneerde <36726026+jpvaneerde@users.noreply.github.com>
2018-02-22 11:35:27 +0100

    Create jpcontact.go

---
+++ jpcontact.go
@@ -0,0 +1,24 @@
+package jpcontact
+
+import ("github.com/mediocregopher/radix.v2/redis"
+	"fmt"
+       )
+type ContactDetails struct {
+	Name      string
+	Age       string
+	FavDrink1 string
+	FavDrink2 string
+}
+
+func AddContact(newContact ContactDetails) {
+	conn, err := redis.Dial("tcp", "10.1.1.21:32769")
+	if err != nil {
+		// log.Fatal(err)
+	}
+	defer conn.Close()
+	resp := conn.Cmd("HMSET", "friend:", "name", newContact.Name, "Age", newContact.Age, "favDrink1", newContact.FavDrink1, "favDrink2", newContact.FavDrink2)
+	if resp.Err != nil {
+		// log.Fatal(resp.Err)
+	}
+	fmt.Println(newContact.Name)
+}
