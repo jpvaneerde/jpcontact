38590988b533aedc87105beb85039c70216f7a7a
jpvaneerde <36726026+jpvaneerde@users.noreply.github.com>
2018-02-22 13:29:41 +0100

    Update jpcontact.go

--- jpcontact.go
+++ jpcontact.go
@@ -33,16 +33,6 @@ func CheckContact(newContact ContactDetails) string {
 	defer conn.Close()
 	hash := "friend:"
 	hash += newContact.Name
-	resp := conn.Cmd("HMGET", hash, "name")
-	if resp.Err != nil {
-		func AddContact(newContact ContactDetails) {
-	conn, err := redis.Dial("tcp", "10.1.1.21:32769")
-	if err != nil {
-		// log.Fatal(err)
-	}
-	defer conn.Close()
-	hash := "friend:"
-	hash += newContact.Name
 	resp := conn.Cmd("HMSET", hash, "name", newContact.Name, "Age", newContact.Age, "favDrink1", newContact.FavDrink1, "favDrink2", newContact.FavDrink2)
 	if resp.Err != nil {
 		resp2 := "free"
