caa69469786f7666390b3f38ad281e56e4bad678
jpvaneerde <36726026+jpvaneerde@users.noreply.github.com>
2018-02-22 13:26:31 +0100

    Update jpcontact.go

--- jpcontact.go
+++ jpcontact.go
@@ -25,3 +25,32 @@ func AddContact(newContact ContactDetails) {
 	resp2 := conn.Cmd("HMGET", hash, "name")
 	fmt.Println(resp2)
 }
+func CheckContact(newContact ContactDetails) string {
+	conn, err := redis.Dial("tcp", "10.1.1.21:32769")
+	if err != nil {
+		// log.Fatal(err)
+	}
+	defer conn.Close()
+	hash := "friend:"
+	hash += newContact.Name
+	resp := conn.Cmd("HMGET", hash, "name")
+	if resp.Err != nil {
+		func AddContact(newContact ContactDetails) {
+	conn, err := redis.Dial("tcp", "10.1.1.21:32769")
+	if err != nil {
+		// log.Fatal(err)
+	}
+	defer conn.Close()
+	hash := "friend:"
+	hash += newContact.Name
+	resp := conn.Cmd("HMSET", hash, "name", newContact.Name, "Age", newContact.Age, "favDrink1", newContact.FavDrink1, "favDrink2", newContact.FavDrink2)
+	if resp.Err != nil {
+		resp2 := "free"
+	} else {
+	resp2 := "exists"
+	}
+	return resp2
+}
+
+
+
